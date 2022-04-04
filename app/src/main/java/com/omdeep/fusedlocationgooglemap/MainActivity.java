99dc42f329124adc38840e277d8222651d14665d
Omdeep <omdeep1990@gmail.com>
2022-04-04 17:06:30 +0530

    Added Change Map type of current location

--- app/src/main/java/com/omdeep/fusedlocationgooglemap/MainActivity.java
+++ app/src/main/java/com/omdeep/fusedlocationgooglemap/MainActivity.java
@@ -5,24 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import com.google.android.gms.tasks.Task;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     //TODO:Initialize variable
     SupportMapFragment supportMapFragment;
     FusedLocationProviderClient client;
+    private GoogleMap mMap;
+//    private Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void getCurrentLocation() {
+    public void getCurrentLocation() {
         //TODO: Initialize task location
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -62,16 +70,23 @@ public class MainActivity extends AppCompatActivity {
         task.addOnSuccessListener(location -> {
             //TODO: When success
             if (location != null) {
+
                 //TODO: Sync MAp & also see the use of lambda in small brackets
                 supportMapFragment.getMapAsync(googleMap -> {
+                    mMap = googleMap;
                     //TODO: Initialize latitude and longitude
                     LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    //TODO: Change map type from here also
+//                    onCreateOptionsMenu(menu);
+                    googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
                     //TODO: Create marker options
                     MarkerOptions options = new MarkerOptions().position(latLng).title("You are here");
 
                     //TODO: Zoom Map
                     googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
                     googleMap.addMarker(options);
+
+
 
                 });
             }
@@ -87,5 +102,39 @@ public class MainActivity extends AppCompatActivity {
                 getCurrentLocation();
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.map_options, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Change the map type based on the user's selection.
+        switch (item.getItemId()) {
+            case R.id.normal_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                return true;
+            case R.id.hybrid_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                return true;
+            case R.id.satellite_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                return true;
+            case R.id.terrain_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap ) {
+
     }
 }
